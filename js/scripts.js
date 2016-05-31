11ae24b1c5412292e44737cf4ac703afd0b75b36
Igor Seleznov and Reece Jones <igor@>
2016-05-31 11:51:42 -0700

    Add Celsius to Farenheit converter

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,24 @@
+var tempConverter = function(celsius) {
+  return celsius * 9/5 +32;
+}
+
+var celsius = parseFloat(prompt("Enter degrees in Celsius:"));
+
+var result6 = tempConverter (celsius);
+alert(result6);
+
+
+/*
+var bmi = function(weight, hight) {
+  return weight / (hight * hight);
+}
+
+var weight = parseFloat(prompt("Enter your weight:"));
+var hight = parseFloat(prompt("Enter your hight:"));
+var result5 = bmi (weight, hight);
+alert(result5);
+
+
 var add = function(number1, number2) {
   return number1 + number2;
 };
@@ -33,3 +54,4 @@ var number7 = parseInt(prompt("Enter a number:"));
 var number8 = parseInt(prompt("Enter another number"));
 var result4 = divide (number7, number8);
 alert(result4);
+*/
